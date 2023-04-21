0538695c574df9be6cbbf376e9037104a39e1797
FranN2442 <fgonzalezviejo@cifpfbmoll.eu>
2023-04-21 10:54:52 +0200

    Connection Pool | Pom.xml config

--- src/main/java/ejercicio/bbdd/App.java
+++ src/main/java/ejercicio/bbdd/App.java
@@ -1,13 +1,45 @@
 package ejercicio.bbdd;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
+    private static HikariDataSource dataSource;
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
+    }
+
+    private static void initDatabaseConnectionPool() {
+        HikariConfig hikariConfig = new HikariConfig("/database.properties");
+        dataSource = new HikariDataSource(hikariConfig);;
+    }
+
+    private static void closeDatabaseConnectionPool() {
+        dataSource.close();
+    }
+
+    private static void createData(String nombre, int numero) throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("""
+                        INSERT INTO tipos(nombre, numero)
+                        VALUES (?, ?)
+                    """)) {
+                statement.setString(1, nombre);
+                statement.setInt(2, numero);
+                int rowsInserted = statement.executeUpdate();
+                System.out.println("Rows inserted: " + rowsInserted);
+            }
+        }
     }
 }
---
+++ src/main/java/ejercicio/bbdd/Hikari.java
@@ -0,0 +1,5 @@
+package ejercicio.bbdd;
+
+public class Hikari {
+
+}
